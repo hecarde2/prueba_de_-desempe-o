62cd80f3b7e1b778f87ac1cb21180eace1ef8d78
Hector Carvajal de los Reyes <hectordico@hotmail.com>
2026-03-30 08:07:34 -0500

    actulización

---
+++ servicio.py
@@ -0,0 +1,108 @@
+def agregar_estudiante(estudiantes):
+    #agregar al estudiante con los datos de este 
+
+    nombre = input("Nombre completo del etsudiante: ")
+    #el try por si hay algún error lo encasule 
+    try:
+        id_del_estudiante = int(input("ingrese el id del estudiante: "))
+        edad_delestudiante = int(input("ingrese la edad estudiante:  "))
+
+    except ValueError:
+        print("Datos inválidos")
+        return
+    
+    estado_del_estudiante = input(" Ingrese el estado actual del estudiantes activo/ inactivo: ").strip()
+    programa = input("Ingrese programa alque pertenece el estudiante: ").strip()
+# edad del estudiante mayor a 0
+# id del estudiante mayor a 0, el 0 como id no es valido
+
+    if edad_delestudiante < 0 or id_del_estudiante < 0:
+        print("Valores negativos no permitidos")
+
+        return
+# guardar datos del estudiante
+    estudiante = {
+        "nombre": nombre,
+        "id": id_del_estudiante,
+        "edad": edad_delestudiante,
+        "estado": estado_del_estudiante,
+        "programa": programa
+    }
+    
+    estudiantes.append(estudiante)
+    print("agregado correctamente")
+
+
+    
+def mostrar_menu():
+ print("\n========= MENÚ ESTUDIANTES =========")
+ print("1. Registrar nuevos estudiantes.")
+ print("2. Consultar la lista de estudiantes.")
+ print("3. Buscar un estudiante por el nombre")
+ print("4.Actualizar la información de un estudiante.")
+ print("5. Eliminar estudiantes.")
+ print("6. Guardar CSV")
+ print("7. Cargar CSV")
+ print("8. Salir")
+
+
+
+def mostrar_estudiantes(estudiantes):
+
+    if not estudiantes:
+        print("No hay estudiantes registrados")
+        return
+
+    print("\n ESTUDIANTES")
+    for p in estudiantes:
+        print(f"{p['nombre']} | {p['id']} | {p['edad']} | {p['estado']} | {p['programa']}")
+
+    
+def buscar_estudiante(estudiantes):
+
+    nombre = input("ingrese el nombre completo del estudiante, como lo registro:  ").strip()
+
+    for p in estudiantes:
+        if p["nombre"].lower() == nombre.lower():
+            print("estudiante encontrado:")
+            print(p)
+            return
+
+    print("estudiante no encontrado")
+
+
+    # ACTUALIZAR ESTUDIANTE
+
+def actualizar_estudiante(estudiantes):
+
+    nombre = input("Actualizar estudiantes: ").strip()
+
+    for p in estudiantes:
+        if p["nombre"].lower() == nombre.lower():
+
+            try:
+                id_del_estudiante = int(input("Nuevo id del estudiante: "))
+                edad_delestudiante = int(input("Nueva edad del estudiante: "))
+            except ValueError:
+                print("Datos inválidos")
+                return
+
+            if id_del_estudiante < 0 or edad_delestudiante < 0:
+                print("Valores negativos no permitidos")
+                return
+
+            p["id"] = id_del_estudiante
+            p["edad"] = edad_delestudiante
+
+            print("datos actualizados")
+            return
+
+    print("estudiante no encontrado")
+
+    
+
+
+
+
+
+
--- student-system.py
+++ student-system.py
@@ -1,9 +1,37 @@
 
-from  services import *
+from  servicio import *
 print("Welcome student system")
 
-Estudiantes = []
+estudiantes = []
 option = 0
     
-while option != 6:
-    
+while option != 8:
+    mostrar_menu()
+
+    try:
+        option = int(input("Ingrese una opción (1-8): "))
+
+        if option < 1 or option > 8:
+            print("Opción fuera de rango.")
+            continue
+
+    except ValueError:
+        print("Debe ingresar un número.")
+        continue
+
+
+#AGREGAR UN ESTUDIANTE
+    if option == 1:
+        agregar_estudiante(estudiantes)
+
+    if option == 2:
+        mostrar_estudiantes(estudiantes)
+
+    if option == 3:
+        buscar_estudiante(estudiantes)
+
+    if option == 4:
+        actualizar_estudiante(estudiantes)
+
+
+
